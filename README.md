0ff33da3921e90713f4914fb2ee0442c3b54d7db
ct <ct@pop-os.localdomain>
2022-08-03 18:45:05 -0500

    readme edit

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # eltrello
+
+
+**Build of fullstack Trello clone**
+
+from [Oleksandr Kocherin's Udemy course](https://www.udemy.com/course/build-fullstack-trello-clone-websocket-socket-io)
---
+++ server/src/server.ts
@@ -0,0 +1,37 @@
+import express from 'express';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
+import mongoose from 'mongoose';
+import * as usersController from "./controllers/users";
+import bodyParser from 'body-parser';
+import authMiddleware from './middlewares/auth';
+import cors from 'cors';
+
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer);
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.send('API is up, dog!');
+});
+
+app.post('/api/users', usersController.register);
+app.post('/api/users/login', usersController.login);
+app.get('/api/user', authMiddleware, usersController.currentUser);
+
+io.on('connection', () => {
+    console.log('a user connected');
+});
+
+mongoose.connect('mongodb://localhost:27017/eltrello').then(() => {
+    console.log('connected to mongo');
+
+    httpServer.listen(4001, () => {
+        console.log('Server is listening on port 4001');
+    });
+});
+
